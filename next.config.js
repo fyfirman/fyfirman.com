9541538b8764f8d6253a0bd8f540ebd493f8f785
Firmansyah Yanuar <fyfirman@gmail.com>
2021-12-19 21:14:03 +0700

    fix: config webpack

--- next.config.js
+++ next.config.js
@@ -7,6 +7,17 @@ module.exports = {
       loader: "frontmatter-markdown-loader",
       options: { mode: ["react-component"] },
     });
+    cfg.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        // test: /\.(js|ts)x?$/,
+        // for webpack 5 use
+        and: [/\.(js|ts)x?$/],
+      },
+
+      use: ["@svgr/webpack"],
+    });
+
     return cfg;
   },
   images: {
