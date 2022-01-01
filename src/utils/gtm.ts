1c25c29ce066483847fbd7ebc8cf81d1d900ee1d
Firmansyah Yanuar <fyfirman@gmail.com>
2022-01-01 21:43:54 +0700

    fix: remove whitespace on gtag

--- src/utils/gtm.ts
+++ src/utils/gtm.ts
@@ -19,27 +19,14 @@ export const GTMPageView = (url: string) => {
 
 export const getScriptGTM = () => {
   if (!!env.firebaseConfig.gtmAuth && !!env.firebaseConfig.gtmEnvId) {
-    return `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl+'&gtm_auth=
-            ${env.firebaseConfig.gtmAuth}&gtm_preview=env-${env.firebaseConfig.gtmEnvId}
-            &gtm_cookies_win=x';f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-WMCCR8J');`;
+    return `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl+'&gtm_auth=${env.firebaseConfig.gtmAuth}&gtm_preview=env-${env.firebaseConfig.gtmEnvId}&gtm_cookies_win=x';f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WMCCR8J');`;
   }
-  return `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','${env.firebaseConfig.gtmId}');`;
+  return `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${env.firebaseConfig.gtmId}');`;
 };
 
 export const getNoScriptGTM = () => {
   if (!!env.firebaseConfig.gtmAuth && !!env.firebaseConfig.gtmEnvId) {
-    return `<iframe src="https://www.googletagmanager.com/ns.html?id=${env.firebaseConfig.gtmId}
-            &gtm_auth=${env.firebaseConfig.gtmAuth}&gtm_preview=env-${env.firebaseConfig.gtmEnvId}&gtm_cookies_win=x"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+    return `<iframe src="https://www.googletagmanager.com/ns.html?id=${env.firebaseConfig.gtmId}&gtm_auth=${env.firebaseConfig.gtmAuth}&gtm_preview=env-${env.firebaseConfig.gtmEnvId}&gtm_cookies_win=x"height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
   }
-  return `<iframe src="https://www.googletagmanager.com/ns.html?id=${env.firebaseConfig.gtmId}"
-          height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
+  return `<iframe src="https://www.googletagmanager.com/ns.html?id=${env.firebaseConfig.gtmId}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
 };
