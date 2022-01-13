bac73ac6dfd2c24503927581ddbaa22295f1c4b0
Firmansyah Yanuar <fyfirman@gmail.com>
2022-01-13 23:09:03 +0700

    chore: create first test

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,8 @@
-import '@testing-library/jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect';
+
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: () => {
+    return 'Next image stub';
+  },
+}));
---
+++ src/__tests__/pages/index.test.tsx
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@pages/index";
+
+describe("HomePage", () => {
+  it("home page content rendered properly", () => {
+    const { container } = render(<Home />);
+
+    expect(container.firstChild?.hasChildNodes()).toBeTruthy();
+  });
+});
