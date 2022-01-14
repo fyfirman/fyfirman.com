4afe3b1df3535183bfdc4be8b1b9e21cfa56be5a
Firmansyah Yanuar <fyfirman@gmail.com>
2022-01-15 01:41:10 +0700

    feat: add version name

--- src/components/template/footer/footer.tsx
+++ src/components/template/footer/footer.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from "react";
+import { version } from "../../../../package.json";
 import styles from "./footer.module.scss";
 import { clsx } from "~/helpers";
 import useResponsive from "~/hooks/useResponsive";
@@ -8,6 +9,11 @@ const Footer: FC<{}> = () => {
 
   return (
     <footer className={clsx([styles.container, isMobile && styles.mobile])}>
+      {process.env.NODE_ENV !== "production" && (
+        <span className={styles.version}>{`${
+          process.env.NODE_ENV.charAt(0).toUpperCase() + process.env.NODE_ENV.slice(1)
+        } - v${version}`}</span>
+      )}
       <iframe
         className={styles["spline-danbo"]}
         frameBorder="0"
