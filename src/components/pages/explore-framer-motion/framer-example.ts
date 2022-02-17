626a3bf0cdb3822f3e7ab086fb7286022920da9c
Firmansyah Yanuar <fyfirman@gmail.com>
2022-02-17 22:42:05 +0700

    feat: animation controls showcase

--- src/components/pages/explore-framer-motion/framer-example.ts
+++ src/components/pages/explore-framer-motion/framer-example.ts
@@ -1,10 +1,18 @@
 import { FC } from "react";
 import AnimationControlsExample from "./animation-controls";
+import AnimationControlsShowcase from "./animation-controls-showcase";
 import ConstantDelay from "./constant-delay";
 import Unnamed from "./unnamed";
 import Untitled from "./untitled";
 import UseCycle from "./useCycle";
 
-const framerExample: FC[] = [ConstantDelay, Unnamed, Untitled, UseCycle, AnimationControlsExample];
+const framerExample: FC[] = [
+  ConstantDelay,
+  Unnamed,
+  Untitled,
+  UseCycle,
+  AnimationControlsExample,
+  AnimationControlsShowcase,
+];
 
 export default framerExample;
