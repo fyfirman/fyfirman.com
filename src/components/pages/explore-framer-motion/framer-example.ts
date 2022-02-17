6c0e0b072e1d6cacaa66679dffb71d8dbb8e1b89
Firmansyah Yanuar <fyfirman@gmail.com>
2022-02-17 20:18:26 +0700

    feat: add use cycle example

--- src/components/pages/explore-framer-motion/framer-example.ts
+++ src/components/pages/explore-framer-motion/framer-example.ts
@@ -2,7 +2,8 @@ import { FC } from "react";
 import ConstantDelay from "./constant-delay";
 import Unnamed from "./unnamed";
 import Untitled from "./untitled";
+import UseCycle from "./useCycle";
 
-const framerExample: FC[] = [ConstantDelay, Unnamed, Untitled];
+const framerExample: FC[] = [ConstantDelay, Unnamed, Untitled, UseCycle];
 
 export default framerExample;
--- src/pages/explore/framer-motion.tsx
+++ src/pages/explore/framer-motion.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import framerExample from "~/components/pages/explore-framer-motion/framer-example";
 
 const FramerMotion: React.FC = () => {
+  const [isReversed, setIsReversed] = useState(true);
+
   return (
     <main style={{ minHeight: "100vh" }}>
-      {framerExample.map((Component, index) => (
-        <Component key={index} />
-      ))}
+      <button onClick={() => setIsReversed((prev) => !prev)}>
+        Reverse! ({isReversed ? "Newest to Oldest" : "Oldest to newest"})
+      </button>
+      {isReversed
+        ? framerExample.map((Component, index) => <Component key={index} />).reverse()
+        : framerExample.map((Component, index) => <Component key={index} />)}
     </main>
   );
 };
