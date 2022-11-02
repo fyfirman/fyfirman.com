4b20529f45099d9a5154c66db5c8d87f21812c8d
Firmansyah Yanuar <firmansyah.yanuar@autobahn-security.com>
2022-11-03 00:03:05 +0700

    🧑‍💻 feat: handle ssr in `useAudio` hooks

--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -1,6 +1,9 @@
+/* eslint-disable react-hooks/rules-of-hooks -- need to be ignore the rest of code if is it SSR */
 import { useState, useEffect } from "react";
 
 const useAudio = (url: string): [boolean, () => void] => {
+  if (typeof window === "undefined") return [false, () => {}];
+
   const [audio] = useState(new Audio(url));
   const [isPlaying, setIsPlaying] = useState(false);
 
