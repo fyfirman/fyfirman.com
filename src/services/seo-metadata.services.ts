2e039c00e5329ac6b8e7e8fefe5a0649fe3e1f82
Firmansyah Yanuar <firmansyah.yanuar@autobahn-security.com>
2023-08-06 16:35:28 +0700

    fix: lint

--- src/services/seo-metadata.services.ts
+++ src/services/seo-metadata.services.ts
@@ -1,6 +1,12 @@
 import axios, { AxiosResponse } from "axios";
 import env from "~/utils/environment";
-import { SeoMetadata } from "~/pages/api/seo-metadata";
+
+export type SeoMetadata = {
+  title: string;
+  description: string;
+  url: string;
+  icoUrl?: string;
+};
 
 const postGenerate = async (url: string) => {
   const res: AxiosResponse<SeoMetadata> = await axios.post(`${env.scraperServiceUrl}/seo-scraper`, {
