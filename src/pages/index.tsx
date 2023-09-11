89df3ef82268a2858677f2e19ea99c75928e5f01
Firmansyah Yanuar <firmansyah.yanuar@autobahn-security.com>
2023-09-12 03:50:13 +0700

    feat: hide github contrib for mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,16 @@ import HomeWeapon from "~/components/pages/home/home-weapon";
 import HomeProject from "~/components/pages/home/home-project";
 import HomeContact from "~/components/pages/home/home-contact";
 import HomeGithubContributor from "~/components/pages/home/home-github-contributor";
+import useResponsive from "~/hooks/useResponsive";
 
 const Home: NextPage = () => {
+  const { isMobile } = useResponsive();
+
   return (
     <>
       <Head title="Home" />
       <HomeHeader />
-      <HomeGithubContributor />
+      {!isMobile ? <HomeGithubContributor /> : null}
       <HomeWeapon />
       <HomeProject />
       <HomeContact />
